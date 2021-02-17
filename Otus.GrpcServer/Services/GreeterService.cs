c9c7290286611d62ecd2210bfd70a61b9453d8eb
Viktor Nikolaev <viknikolaev@ozon.ru>
2021-02-17 21:00:10 +0300

    http over grpc

--- Otus.GrpcServer/Services/GreeterService.cs
+++ Otus.GrpcServer/Services/GreeterService.cs
@@ -4,37 +4,41 @@ using Microsoft.Extensions.Logging;
 
 namespace Otus.GrpcServer.Services
 {
-  public class GreeterService : Greeter.GreeterBase
-  {
-    private readonly ILogger<GreeterService> _logger;
-
-    public GreeterService(ILogger<GreeterService> logger)
+    public class GreeterService : Greeter.GreeterBase
     {
-      _logger = logger;
-    }
+        private readonly ILogger<GreeterService> _logger;
 
-    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-    {
-      var helloReply = new HelloReply
-      {
-        Message = "Hello " + request.Name
-      };
-
-      return Task.FromResult(helloReply);
-    }
-
-    public override async Task BidiHello(IAsyncStreamReader<HelloRequest> requestStream,
-      IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
-    {
-      await foreach (var request in requestStream.ReadAllAsync())
-      {
-        var helloReply = new HelloReply
+        public GreeterService(ILogger<GreeterService> logger)
         {
-          Message = "Hello " + request.Name
-        };
+            _logger = logger;
+        }
 
-        await responseStream.WriteAsync(helloReply);
-      }
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            var helloReply = new HelloReply
+            {
+                Message = "Hello " + request.Name
+            };
+
+            return Task.FromResult(helloReply);
+        }
+
+        public override async Task BidiHello
+        (
+            IAsyncStreamReader<HelloRequest> requestStream,
+            IServerStreamWriter<HelloReply> responseStream,
+            ServerCallContext context
+        )
+        {
+            await foreach (var request in requestStream.ReadAllAsync())
+            {
+                var helloReply = new HelloReply
+                {
+                    Message = "Hello " + request.Name
+                };
+
+                await responseStream.WriteAsync(helloReply);
+            }
+        }
     }
-  }
 }
--- Otus.GrpcServer/Startup.cs
+++ Otus.GrpcServer/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Otus.GrpcServer.Services;
 
 namespace Otus.GrpcServer
@@ -17,6 +19,45 @@ namespace Otus.GrpcServer
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddGrpc();
+      
+      
+      services.AddGrpcHttpApi();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo {Title = "EAP Metadata HTTP API", Version = "v1"});
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+          Description =
+            "JWT Authorization header using the Bearer scheme. " +
+            "Enter 'Bearer' [space] and then your token in the text input below. " +
+            "Example: 'Bearer 12345abcdef'",
+          Name = "Authorization",
+          In = ParameterLocation.Header,
+          Type = SecuritySchemeType.ApiKey,
+          Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+          {
+            new OpenApiSecurityScheme
+            {
+              Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header
+            },
+            new List<string>()
+          }
+        });
+      });
+
+      services.AddGrpcSwagger();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +86,9 @@ namespace Otus.GrpcServer
         RequestPath = "/proto"
       });
 
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EAP Metadata HTTP API V1"));
+      
       app.UseRouting();
 
       app.UseEndpoints(endpoints =>
